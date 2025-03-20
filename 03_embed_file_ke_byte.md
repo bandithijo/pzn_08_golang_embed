8666b84f4f90472d0ef6b574f98af4c47edbef65
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-03-21 04:21:11 +0800

    embed file ke byte

---
+++ 03_embed_file_ke_byte.md
@@ -0,0 +1,4 @@
+# Embed File ke []byte
+
+- Selain ke tipe data String, embed file juga bisa dilakukan ke variable tipe data []byte
+- Ini cocok sekali jika kita ingin melakukan embed file dalam bentuk binary, seperti gambar dan lain-lain
--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package pzn_08_golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed logo.jpg
+var logo []byte
+
+func TestByteArray(t *testing.T) {
+	err := os.WriteFile("logo_next.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
